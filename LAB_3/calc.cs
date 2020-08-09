2b60c1761b43093797c474949a025b6c0f4ddf58
akki-1211 <65230665+akki-1211@users.noreply.github.com>
2020-08-09 17:54:14 +0530

    Add files via upload

--- LAB_3/calc.cs
+++ LAB_3/calc.cs
@@ -1,14 +1,18 @@
 using System;
+using System.Reflection;
+[assembly:AssemblyVersion("1.0.0.0")]
 namespace cal
 {
     public class calc
     {
         public int addition(int a, int b)
         {
+            Console.WriteLine("New Version");
             return a + b;
         }
         public int multiplication(int a, int b)
         {
+            Console.WriteLine("New Version");
             return a * b;
         }
 
