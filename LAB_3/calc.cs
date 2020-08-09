3ede3fe56e1e509f89a22bc3e7d53d1a6bb12ecf
akki-1211 <65230665+akki-1211@users.noreply.github.com>
2020-08-09 17:53:07 +0530

    Add files via upload

---
+++ LAB_3/calc.cs
@@ -0,0 +1,16 @@
+using System;
+namespace cal
+{
+    public class calc
+    {
+        public int addition(int a, int b)
+        {
+            return a + b;
+        }
+        public int multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+    }
+}
